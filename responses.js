97c2e03f916a93e7f7d0cb4a323d809a5396de1e
amraouiabdrahim <72153047+amraouiabdrahim@users.noreply.github.com>
2023-07-21 14:19:59 +0100

    Add files via upload

---
+++ responses.js
@@ -0,0 +1,16 @@
+function getBotResponse(input) {
+
+    // Simple responses
+    if (input == "salam") {
+        return "wa3alikom salam 😍";
+    } else if (input == "localisation") {
+        return "9isariyat dandan dhaybiya 7da hotel jaza2ir mra7ba bik 😉😊";
+    } else if (input == "taw9it") {
+        return "10:00-20:30";
+    } else if (input == "model") {
+        return "k7el, gry , bleu , grona , K7al bmotif  mra7ba bik 😉😊";
+
+    } else {
+        return "mafhmtkch 😢🤷‍♀️";
+    }
+}
